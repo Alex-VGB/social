c3f0557fc166c698c629f1378ada1a2d50156527
Aleksander <64606852+Alex-VGB@users.noreply.github.com>
2020-07-18 16:43:15 +0200

    Initial commit

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import s from './Dialogs.module.css';
+import DialogItem from "./DialogItem/DialogsItem";
+import Message from "./Message/Message";
+
+const Dialogs = (props) => {
+
+    let state = props.dialogsPage;
+
+    let dialogsElements = state.dialogs.map(d => <DialogItem name={d.name} key={d.id} id={d.id}/>);
+    let messageElements = state.messages.map(m => <Message key={m.id} message={m.message}/>);
+    let newMessageBody = state.newMessageBody;
+
+
+    let onSendMessageClick = () => {
+        props.sendMessage();
+    }
+    let onNewMessageChange = (e) => {
+        let body = e.target.value;
+        props.updateNewMessageBody(body);
+    }
+
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                {dialogsElements}
+            </div>
+            <div className={s.messages}>
+                {messageElements}
+                <div>
+                    <textarea
+                        placeholder='Write Text'
+                        onChange={onNewMessageChange}
+                        value={newMessageBody}/>
+                </div>
+                <div>
+                    <button onClick={onSendMessageClick}>Send</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Dialogs;
