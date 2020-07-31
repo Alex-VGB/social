ea07112ba55bc33ba315a4c421eee2b97511bd3c
Aleksander <64606852+Alex-VGB@users.noreply.github.com>
2020-07-31 22:24:55 +0200

    upload Photo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import s from './Header.module.css';
 import {NavLink} from "react-router-dom";
+import logo from '../../assets/images/head_logo.svg'
+import userPhoto from "../../assets/images/user.png";
 
 const Header = (props) => {
     return <header className={s.header}>
-        <img src='https://www.freelogodesign.org/Content/img/logo-ex-7.png' />
+        <img src={logo}/>
 
         <div className={s.loginBlock}>
             {props.isAuth
-                ? <div>{props.login} - <button onClick={props.logout}>Log out</button></div>
+                ? <div className={s.colorLogin}>{props.login}
+                    <img src={props.profile.photos.large || userPhoto} className={s.mainPhotoSmall} alt=""/>
+                    <button className={s.loginBtn} onClick={props.logout}>Log out</button>
+                </div>
                 : <NavLink to={'/login'}>Login</NavLink>
             }
         </div>
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,13 +3,13 @@ import Profile from "./Profile";
 import {connect} from "react-redux";
 import {withRouter} from "react-router-dom";
 import {compose} from "redux";
-import {getStatus, getUserProfile, updateStatus} from "../../redux/profile-reducer";
+import {getStatus, getUserProfile, savePhoto, updateStatus} from "../../redux/profile-reducer";
 import {WithAuthRedirect} from "../../hoc/withAuthRedirect";
 
 
 class ProfileContainer extends React.Component {
 
-    componentDidMount() {
+    refreshProfile () {
         let userId = this.props.match.params.userId;
         if (!userId) {
             userId = this.props.authorizedUserId;
@@ -21,12 +21,23 @@ class ProfileContainer extends React.Component {
         this.props.getStatus(userId);
     }
 
+    componentDidMount() {
+        this.refreshProfile();
+    }
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.match.params.userId !== prevProps.match.params.userId) {
+            this.refreshProfile();
+        }
+    }
+
     render() {
         return (
             <Profile {...this.props}
+                isOwner={!this.props.match.params.userId}
                      profile={this.props.profile}
                      status={this.props.status}
-                     updateStatus={this.props.updateStatus}/>
+                     updateStatus={this.props.updateStatus}
+                     savePhoto={this.props.savePhoto}/>
         )
     }
 }
@@ -39,7 +50,7 @@ let mapStateToProps = (state) => ({
 });
 
 export default compose(
-    connect(mapStateToProps, {getUserProfile, getStatus, updateStatus}),
+    connect(mapStateToProps, {getUserProfile, getStatus, updateStatus, savePhoto}),
     withRouter,
     WithAuthRedirect
 )(ProfileContainer);
